9e132c97eb2685ad336184bb8b6585728b789025
Savan Fami <savanfami1239@gmail.com>
2025-01-07 15:10:27 +0530

    fix: fix in cors

--- server/url-shortner/src/main.ts
+++ server/url-shortner/src/main.ts
@@ -2,16 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as morgan from 'morgan';
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Access environment variables using ConfigService
+  const configService = app.get(ConfigService);
+  const url = configService.get<string>('CLIENT');
+  console.log(url)
   app.use(morgan('tiny'));
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: url,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 
-  app.useGlobalPipes(new ValidationPipe({whitelist:true}))
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
